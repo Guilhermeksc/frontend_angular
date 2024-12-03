527f4220dc1745c31f59f8b9c9c94ed53012a823
Guilherme <guilhermekscampos@gmail.com>
2024-12-02 21:33:10 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 
-// Standalone components não são declarados aqui.
 @NgModule({
   imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
-      { path: 'atas', loadComponent: () => import('./components/atas/atas.component').then(m => m.AtasComponent) },
-      { path: 'licitacao', loadComponent: () => import('./components/licitacao/licitacao.component').then(m => m.LicitacaoComponent) },
-      { path: 'contratos', loadComponent: () => import('./components/contratos/contratos.component').then(m => m.ContratosComponent) },
-      { path: 'dispensa-eletronica', loadComponent: () => import('./components/dispensa-eletronica/dispensa-eletronica.component').then(m => m.DispensaEletronicaComponent) }
-    ])
+    BrowserModule
   ],
-  bootstrap: [] // AppComponent será configurado em bootstrapApplication
+  bootstrap: [] // Se estiver usando standalone components, não precisa declarar bootstrap aqui.
 })
 export class AppModule {}
